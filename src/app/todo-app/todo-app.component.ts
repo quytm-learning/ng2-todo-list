bcdecc0f2c9a0c536d3b8811e8f3ef22b1ab8f78
quytm <quytm_58@vnu.edu.vn>
2016-09-22 13:33:07 +0700

    fix error todo-component

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TodoService} from "../todo.service";
 import {Todo} from "../todo";
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-todo-app',
-  templateUrl: './todo-app.component.html',
-  styleUrls: ['./todo-app.component.css'],
-  providers: [TodoService]
+    selector: 'app-todo-app',
+    templateUrl: './todo-app.component.html',
+    styleUrls: ['./todo-app.component.css'],
+    providers: [TodoService]
 })
-export class TodoAppComponent implements OnInit {
-  newTodo: Todo = new Todo();
+export class TodoAppComponent {
+    newTodo: Todo = new Todo();
 
-  constructor(private todoService: TodoService) { }
+    constructor(private todoService: TodoService) {
+    }
 
-  ngOnInit() {
-  }
+    addTodo() {
+        this.todoService.addTodo(this.newTodo);
+        this.newTodo = new Todo();
+    }
 
-  addTodo(){
-    this.todoService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
-  }
-  toggleTodoComplete(todo){
-    this.todoService.toggleTodoComplete(todo);
-  }
-  removeTodo(todo){
-    this.todoService.deleteTodoById(todo.id);
-  }
-  getTodos(){
-    return this.todoService.getAllTodo();
-  }
+    toggleTodoComplete(todo) {
+        this.todoService.toggleTodoComplete(todo);
+    }
+
+    removeTodo(todo) {
+        this.todoService.deleteTodoById(todo.id);
+    }
+
+    get todos() {
+        return this.todoService.getAllTodo();
+    }
 
 }
