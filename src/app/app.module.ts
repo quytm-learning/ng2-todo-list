d3f7d6f50e93c338856be3319e3eb4fa39e859d6
quytm <quytm_58@vnu.edu.vn>
2016-10-04 11:57:32 +0700

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { TodoAppComponent } from './todo-app/todo-app.component';
+import {AppComponent} from './app.component';
+import {TodoAppComponent} from './todo-app/todo-app.component';
+import {HeaderComponent} from './header/header.component';
+import {TodoDetailComponent} from './todo-detail/todo-detail.component';
+import {routing} from "./app.routing";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoAppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TodoAppComponent,
+        HeaderComponent,
+        TodoDetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import {Routes, RouterModule} from "@angular/router";
+import {TodoDetailComponent} from "./todo-detail/todo-detail.component";
+import {ModuleWithProviders} from "@angular/core";
+import {TodoAppComponent} from "./todo-app/todo-app.component";
+/**
+ * Created by tmq on 27/09/2016.
+ */
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: TodoAppComponent
+    },
+    {
+        path: 'todo',
+        component: TodoDetailComponent
+    }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
